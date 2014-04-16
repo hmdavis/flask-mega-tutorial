9b36a85805dbd43ea2ca69518c59f186c4e5940d
Harry <harrisonmaxwelldavis@gmail.com>
2014-04-15 21:43:30 -0400

    part 7

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-CSRF_ENABLED = True 
+CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
 OPENID_PROVIDERS = [
@@ -10,8 +10,15 @@ OPENID_PROVIDERS = [
     { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
     { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
     { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
-
-# path to database file 
+    
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-# folder where we store migration data files 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+# mail server settings
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+# administrator list
+ADMINS = ['you@example.com']
--- db_migrate.py
+++ db_migrate.py
@@ -1,18 +1,9 @@
-import imp 
-from migrate.versioning import api 
-from app import db 
+#!flask/bin/python
+import imp
+from migrate.versioning import api
+from app import db
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
-
-"""
-1) compare structure of databse to structure of models
-2) differences between the two are recorded as migration script in repo 
-3) script knows how to apply migration or undo it so we can up/downgrade 
-
-tip: never rename existing fields, try to add/remove models or fields 
-tip: always run migration first on development database, not production 
-tip: always have a backup before you migrate 
-"""
 migration = SQLALCHEMY_MIGRATE_REPO + '/versions/%03d_migration.py' % (api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO) + 1)
 tmp_module = imp.new_module('old_model')
 old_model = api.create_model(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
